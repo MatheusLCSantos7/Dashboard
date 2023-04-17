e446d1537e91abd4f9e51f74bd31a24b6896f3dd
Matheus LC Santos <matheuslcsantosbh@gmail.com>
2023-04-17 12:50:53 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber : '85631',
+        paymenteStatus : 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'i5-11400f',
+        productNumber : '1412',
+        paymenteStatus : 'Refuded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'i5-11400g',
+        productNumber : '85631',
+        paymenteStatus : 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'i5-12400f',
+        productNumber : '4231',
+        paymenteStatus : 'Paid',
+        shipping: 'warning'
+    },
+    {
+        productName: 'i5-12600f',
+        productNumber : '6353',
+        paymenteStatus : 'Paid',
+        shipping: 'Delivered'
+    }
+];
